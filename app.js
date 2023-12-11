f45c1d9d7d08e44a3de35fefefd2e3c42f7fd492
Alex <gli0471@hotmail.com>
2023-12-11 18:53:28 +1100

    send email

--- app.js
+++ app.js
@@ -1,32 +1,26 @@
 const express = require('express');
-const app = express();
+const emailjs = require('@emailjs/nodejs');
+var app = express();
+app.use(express.json());
 
-app.get('/api/sendEmail', (req, res) => {
-    // code fragment
-    var data = {
-        service_id: 'default_service',
-        template_id: 'template_4jy2yu9',
-        user_id: 'cRvLZ3RUw3QmirEFF',
-        template_params: {
-            'name': req.body.name,
-            'email': req.body.email,
-            'phoneNumber': req.body.phoneNumber,
-            'message': req.body.message
-        }
+app.post('/api/sendEmail', (req, res) => {
+
+    var templateParams = {
+        name: req.body.name,
+        email: req.body.email,
+        phoneNumber: req.body.phoneNumber,
+        message: req.body.message
     };
-    
-    fetch('https://api.emailjs.com/api/v1.0/email/send', {
-        method: 'POST',
-        data: JSON.stringify(data),
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-    }).then(function() {
-        console.log('Your mail is sent!');
-    }).catch(function(error) {
-        console.log('Oops... ' + JSON.stringify(error));
-    });
+    emailjs.send('default_service','template_fu48g3t', templateParams, {
+        publicKey: 'cYszRWLG4VIoLB7uV',
+      })
+        .then(function(response) {
+           console.log('SUCCESS!', response.status, response.text);
+           res.send('ok')
+        }, function(err) {
+           console.log('FAILED...', err);
+           res.send('fail')
+        });
 });
 
 app.listen(3000, ()=> console.log('Server is running on port 3000'))
