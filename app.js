e07f53a69e1c52a4dc0d1294275f5d9f7bbd94b1
fcai0003 <fcai0003@student.monash.edu>
2023-12-13 18:44:53 +1100

    update user data

--- app.js
+++ app.js
@@ -29,10 +29,48 @@ app.post('/api/sendEmail', async (req, res) => {
            res.send('fail')
         });
 });
+//update data
 
+app.post('/api/updateNotification', async (req, res) => {
+    var updatedNotification = {
+        name: req.body.name,
+        email: req.body.email,
+        phoneNumber: req.body.phoneNumber,
+        message: req.body.message
+    };
+    let existingNotification = await Notification.find({name:updatedNotification.name});
+
+    if (!existingNotification){
+        return res.status(404).send("can not find notifications");
+    }
+    await Notification.updateOne({name:updatedNotification.name}, updatedNotification)
+    return res.status(200).send("success");
+})
+//Find data
+app.get('/api/getNotificationsByName/:name', async (req, res) => {
+    const {name} = req.params;
+    const allNotifcations = await Notification.find({name:name});
+    return res.status(200).json(allNotifcations);
+})
+//Find data
 app.get('/api/getNotifications', async (req, res) => {
     const allNotifcations = await Notification.find();
     return res.status(200).json(allNotifcations);
+})
+
+
+app.delete('/api/deleteNotificationsByName/:name', async (req, res) => {
+    const {name} = req.params;
+    let existingNotification = await Notification.find({name:name});
+
+    if (!existingNotification || existingNotification.length == 0){
+        
+        return res.status(404).send("can not find notifications");
+    }
+    
+   await Notification.deleteOne({name:name});
+   return res.status(200).send("success");
+
 })
 
 //connect to database
